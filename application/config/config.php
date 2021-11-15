9911d569f62c1b6cda4918f2ec8dd66dc2879811
Suresh Subramani <sureshsubramani1986@gmail.com>
2021-11-15 15:39:51 +0530

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:81/react/freindswhereapi/';
+$config['base_url'] = 'https://friendswhereapi.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------

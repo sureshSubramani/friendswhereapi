60778a3a5a3abbecb10299085a8ce51f4586c561
Suresh Subramani <sureshsubramani1986@gmail.com>
2021-11-15 15:40:52 +0530

    Add files via upload

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -49,13 +49,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		margin: 0 15px 0 15px;
 	}
 
-	p.footer {
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
+
+	.text-center{
+		margin:0 auto;
+		text-align:center;
 	}
 
 	#container {
@@ -67,22 +64,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 </head>
 <body>
 
-<div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
+<div id="container" class="text-center">
+	<h1>Welcome to API</h1>
 
 	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+		<p>**************************************************************</p>
+		<p>**************************************************************</p>
+		<p>**************************************************************</p>
+		<h1>********************* FriendsWhere API ************************</h1>
+		<p>**************************************************************</p>
+		<p>**************************************************************</p>
+		<p>**************************************************************</p>
 	</div>
 
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+	
 </div>
 
 </body>
